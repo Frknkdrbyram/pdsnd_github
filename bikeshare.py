999fefd218207f00e585f6eb9943872d565e9dc2
Furkan BAYRAM <Furkan-Kadir.BAYRAM@shell.com>
2023-03-03 16:35:49 +0100

    Multiply() Function called in main

--- bikeshare.py
+++ bikeshare.py
@@ -258,6 +258,7 @@ def main():
         trip_duration_stats(df)
         user_stats(df)
         individual_data(df)
+        multiply(5,5)
         
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
